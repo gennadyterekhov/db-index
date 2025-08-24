613676a8746c818b6b77e12a1f4d2709c0327545
Gennady Terekhov <ter.ge@yandex.ru>
2025-08-24 19:24:32 +0300

    init

--- src/Dto/QueryAnalysisResult.php
+++ src/Dto/QueryAnalysisResult.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Dto;
 
-final readonly class AnalysisResult
+final readonly class QueryAnalysisResult
 {
     public function __construct(
         public string $sql,
