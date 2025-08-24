51343dfab7a9917c0f77ca55fe7dbd7a4d80e056
Gennady Terekhov <ter.ge@yandex.ru>
2025-08-24 17:49:47 +0300

    init

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,18 +2,32 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserEmailBTreeRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class MainController
+class MainController extends AbstractController
 {
+    public function __construct(
+        private UserRepository $userRepository,
+        private UserEmailBTreeRepository $userEmailBTreeRepository,
+    ) {}
+
     #[Route('/')]
     public function main(): Response
     {
         $number = random_int(0, 100);
 
+        $allUsers = $this->userRepository->findAll();
+        $allUsersEmailBtree = $this->userEmailBTreeRepository->findAll();
+        $allUsers = array_map(static fn(User $u) => $u->__toArray(), $allUsers);
+
+        $dt = json_encode($allUsers);
         return new Response(
-            '<html><body>Lucky number: ' . $number . '</body></html>'
+            '<html><body>Lucky number: ' . $number . " <pre>$dt</pre></body></html>"
         );
     }
 }
