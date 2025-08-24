8fa52a41cace111a15c72f60ccfdd71412a0c686
Gennady Terekhov <ter.ge@yandex.ru>
2025-08-24 17:40:30 +0300

    init

--- src/Command/CreateUsersCommand.php
+++ src/Command/CreateUsersCommand.php
@@ -6,22 +6,28 @@ namespace App\Command;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Faker\Factory;
 use Faker\Generator;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(name: 'app:create-users')]
-final readonly class CreateUsersCommand
+final class CreateUsersCommand extends Command
 {
     public const int NUMBER_OF_USERS = 1000;
+    private Generator $faker;
 
     public function __construct(
         private EntityManagerInterface $entityManager,
-        private Generator $faker,
-    ) {}
+    ) {
+        parent::__construct();
+    }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $this->faker = Factory::create();
         $this->createUsers();
         return Command::SUCCESS;
     }
