d48c5ff5390fe61246afb72af0b8950c1e3bc741
Gennady Terekhov <ter.ge@yandex.ru>
2025-08-24 20:16:34 +0300

    init

--- src/Services/AnalyzeTable.php
+++ src/Services/AnalyzeTable.php
@@ -8,17 +8,18 @@ use App\Dto\TableAnalysisResult;
 
 final readonly class AnalyzeTable
 {
+    public const array METHODS = [
+        'findAll',
+        'findByEmail',
+    ];
+
     public function __construct(
         private AnalyzeQuery $analyzeQuery,
     ) {}
 
     public function getAnalysisAndData(string $class, array $methods = []): TableAnalysisResult
     {
-        $methods = (count($methods) === 0)
-            ? [
-                'findAll',
-            ]
-            : $methods;
+        $methods = (count($methods) === 0) ? self::METHODS : $methods;
         $analyses = [];
         foreach ($methods as $method) {
             $analyses[] = $this->analyzeQuery->getAnalysisAndData($class, $method);
